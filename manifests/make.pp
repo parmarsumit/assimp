33236cece2abb4d4dd872b96bc8391a818332a33
Sandy <sandeep@panthacorp.com>
2016-02-14 22:37:33 +0530

    removed archive extension

--- manifests/make.pp
+++ manifests/make.pp
@@ -3,7 +3,7 @@ class assimp::make {
 	exec{ "make assimp":
     		command => "bash -c \"cmake CMakeLists.txt -G 'Unix Makefiles' && make\"",
     		logoutput => 'true',
-    		cwd => "/opt/assimp-3.1.1/",
-		creates => "/opt/assimp-3.1.1/bin/assimp"
+    		cwd => "/opt/assimp/",
+		creates => "/opt/assimp/bin/assimp"
   	}
 }

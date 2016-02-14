e817893a9fdc08da07bf4a1825a1aa9c1ff24329
Sandy <sandeep@panthacorp.com>
2016-02-14 22:05:24 +0530

    created soft link for version control

--- manifests/source.pp
+++ manifests/source.pp
@@ -1,14 +1,20 @@
 class assimp::source(
 	$version = "",
 	$assimpurl = ""
-) 
-{ 
+)
+{
 	archive { "assimp-${version}":
-  		ensure => present,
-  		url    => "$assimpurl",
-  		target => '/opt',
+  	ensure => present,
+  	url    => "$assimpurl",
+  	target => '/opt',
 		extension => 'zip',
 		checksum => false,
 		timeout   => "1800"
+	}
+
+	file { '/opt/assimp':
+		ensure  => link,
+		target  => "/opt/assimp-${version}"",
+		require => Archive["assimp-${version}"],
 	}
 }

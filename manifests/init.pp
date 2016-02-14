e9f6ddac3a13c9fcc640daf78895f4960b23c5dd
sandy724 <sandeep@gmail.com>
2016-02-14 16:20:25 +0000

    changed version url

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,45 +1,8 @@
-# == Class: assimp
-#
-# Full description of class assimp here.
-#
-# === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
-#
-# === Examples
-#
-#  class { assimp:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
-#
-# === Authors
-#
-# Author Name <author@domain.com>
-#
-# === Copyright
-#
-# Copyright 2016 Your name here, unless otherwise noted.
-#
 class assimp (
-	$version 	= "3.1",
-	$srcversion	= undef,
-	$dependencies	= [ "build-essential","zlib1g-dev","unzip","cmake" ],
-	$url		= "http://nchc.dl.sourceforge.net/project/assimp"
+        $version        = "3.1",
+        $srcversion     = undef,
+        $dependencies   = [ "build-essential","zlib1g-dev","unzip","cmake" ],
+        $url            = "https://github.com/assimp/assimp/archive/"
 ){
 Exec {
     path => [
@@ -53,24 +16,16 @@ Exec {
   }
 
  dependencies{ $dependencies:
-	before => Class["assimp::source"]
-	}
- 
-   if $srcversion != undef {
-    $src_version = $srcversion
-  } else {
-    $src_version = $version? {
-      '3.1' => '3.1.1'
-    }
-  }
+        before => Class["assimp::source"]
+        }
 
-	$assimpurl="http://nchc.dl.sourceforge.net/project/assimp/assimp-$version/assimp-$src_version.zip"
-	
-	class { assimp::source:
-		version   => $version,
-		assimpurl => $assimpurl,
-	}
-	class { assimp::make: 
-		require => Class["assimp::source"]
-	}
+        $assimpurl="${url}/v${version}.tar.gz"
+
+        class { assimp::source:
+                version   => $version,
+                assimpurl => $assimpurl,
+        }
+        class { assimp::make:
+                require => Class["assimp::source"]
+        }
 }
